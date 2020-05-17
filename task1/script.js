ac493358114a92251e96d9d0f1e0ecf2cf93de66
Yaroslav Pasko <yaroslavpasko81@gmail.com>
2020-05-17 11:59:10 +0300

    Add folder task1 with index.html script.js

---
+++ task1/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let money = +prompt("Введіть ваш місячний бюджет"),
+    time = prompt("Введіть дату в форматі РРРР-MM-ДД");
+
+console.log(money);
+console.log(time);
+
+let firstAnswer = prompt("Введіть обовязкову статтю витрат");
+let secondAnswer = +prompt("В скільки обійдеться");
+
+console.log(firstAnswer);
+console.log(secondAnswer);
+
+let appDate = {
+    budjet: money,
+    dataTime: time,
+    expenses: {
+        "firstAnswer": "secondAnswer"
+    },
+    optionalExpenses: {
+
+    },
+    income: [],
+    savings: false
+
+};
+
+
+
+alert("Бюджет на один день " + money / 30);
